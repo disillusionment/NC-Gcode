5419e7264958601f5d0eb199ec4811672d1de68e
Eli Cikowski <disillusionment@gmail.com>
2021-09-06 15:24:26 -0700

    Cleaned up files and some fixes
    Removed Unnecessary Template files from Atom package template
    Added ; comment highlighting
    Added XYZE highlighting
    Fixed number highlighting

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # nc-gcode package
 
 Gcode Syntax highlighter for Atom
+3D Printer Specific
--- spec/nc-gcode-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import NcGcodeView from '../lib/nc-gcode-view';
-
-describe('NcGcodeView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
